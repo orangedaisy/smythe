072138555bda92d79e440560636b8d934c6bdaf6
Robert Showalter <robert@orangedaisy.co>
2015-08-02 15:03:40 -0400

    Change page markup

--- themes/smythe/footer.php
+++ themes/smythe/footer.php
@@ -9,16 +9,13 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'smythe' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'smythe' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'smythe' ), 'smythe', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer id="colophon" class="site-footer" role="contentinfo">
+  <div class="site-info">
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'smythe' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'smythe' ), 'WordPress' ); ?></a>
+    <span class="sep"> | </span>
+    <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'smythe' ), 'smythe', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+  </div><!-- .site-info -->
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
--- themes/smythe/header.php
+++ themes/smythe/header.php
@@ -20,23 +20,14 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'smythe' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'smythe' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
-			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-		</div><!-- .site-branding -->
+    <h2 class="wordmark"><?php bloginfo( 'name' ); ?></h2>
+    <h3 class="tagline"><?php bloginfo( 'description' ); ?></h3>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
+		<nav id="site-navigation" class="site-nav" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'smythe' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
