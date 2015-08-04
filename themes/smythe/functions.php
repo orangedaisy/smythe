3ceb9d079c65d05724ab2011d21fad42eb42b6dc
Robert Showalter <robert@orangedaisy.co>
2015-08-04 13:44:05 -0400

    Add secondary menu for commerce items

--- themes/smythe/functions.php
+++ themes/smythe/functions.php
@@ -43,6 +43,7 @@ function smythe_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary Menu', 'smythe' ),
+    'secondary' => esc_html__( 'Secondary Menu', 'smythe' ),
 	) );
 
 	/*
