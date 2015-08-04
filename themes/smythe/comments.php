0bd9bf2f6f5e90433c8ca9bdddfc75b85ae172da
Robert Showalter <robert@orangedaisy.co>
2015-08-04 14:11:53 -0400

    Style single post and comments sections

--- themes/smythe/comments.php
+++ themes/smythe/comments.php
@@ -18,7 +18,7 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="o-paper c-letter comments-area">
 
 	<?php // You can start editing here -- including this comment! ?>
 
