d308e1add91b1b8d68a949230b32133158fd974c
Robert Showalter <robert@orangedaisy.co>
2015-08-04 21:20:35 -0400

    Remove unused div

--- themes/smythe/woocommerce/content-single-product.php
+++ themes/smythe/woocommerce/content-single-product.php
@@ -42,27 +42,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 		 * @hooked woocommerce_show_product_sale_flash - 10
 		 * @hooked woocommerce_show_product_images - 20
 		 */
-		do_action( 'woocommerce_before_single_product_summary' );
+		//do_action( 'woocommerce_before_single_product_summary' );
 	?>
 
-	<div class="summary entry-summary">
-
-		<?php
-			/**
-			 * woocommerce_single_product_summary hook
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-		?>
-
-	</div><!-- .summary -->
+  <?php
+    /**
+     * woocommerce_single_product_summary hook
+     *
+     * @hooked woocommerce_template_single_title - 5
+     * @hooked woocommerce_template_single_rating - 10
+     * @hooked woocommerce_template_single_price - 10
+     * @hooked woocommerce_template_single_excerpt - 20
+     * @hooked woocommerce_template_single_add_to_cart - 30
+     * @hooked woocommerce_template_single_meta - 40
+     * @hooked woocommerce_template_single_sharing - 50
+     */
+    do_action( 'woocommerce_single_product_summary' );
+  ?>
 
 	<?php
 		/**
