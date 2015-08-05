2ba009a216596d20ee8d42b0002fbbac11272b3c
Robert Showalter <robert@orangedaisy.co>
2015-08-04 22:25:31 -0400

    Remove unused page elements

--- themes/smythe/woocommerce/content-single-product.php
+++ themes/smythe/woocommerce/content-single-product.php
@@ -35,16 +35,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div itemscope itemtype="<?php echo woocommerce_get_product_schema(); ?>" id="product-<?php the_ID(); ?>" <?php post_class( 'o-paper c-letter' ); ?>>
 
-	<?php
-		/**
-		 * woocommerce_before_single_product_summary hook
-		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
-		 */
-		//do_action( 'woocommerce_before_single_product_summary' );
-	?>
-
   <?php
     /**
      * woocommerce_single_product_summary hook
@@ -57,19 +47,14 @@ if ( ! defined( 'ABSPATH' ) ) {
      * @hooked woocommerce_template_single_meta - 40
      * @hooked woocommerce_template_single_sharing - 50
      */
-    do_action( 'woocommerce_single_product_summary' );
+    //do_action( 'woocommerce_single_product_summary' );
   ?>
 
-	<?php
-		/**
-		 * woocommerce_after_single_product_summary hook
-		 *
-		 * @hooked woocommerce_output_product_data_tabs - 10
-		 * @hooked woocommerce_upsell_display - 15
-		 * @hooked woocommerce_output_related_products - 20
-		 */
-		do_action( 'woocommerce_after_single_product_summary' );
-	?>
+  <h2 style="margin-top:0;">Mystery Overview</h2>
+  <?php the_excerpt(); ?>
+
+  <h2>Character List</h2>
+  <?php the_content(); ?>
 
 	<meta itemprop="url" content="<?php the_permalink(); ?>" />
 
