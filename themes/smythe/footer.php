66449b349d8f1420cab6764e930371e610b31cc0
Robert Showalter <robert@orangedaisy.co>
2015-08-05 16:04:14 -0400

    Add footer menu

--- themes/smythe/footer.php
+++ themes/smythe/footer.php
@@ -12,6 +12,11 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="site-info">
     <p>&copy;<?php echo date('Y'); ?> Cordelia Smythe Murder Mysteries</p>
+    <nav class="site-nav" role="navigation">
+      <div class="site-nav__wrapper--left">
+        <?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu' ) ); ?>
+      </div>
+    </nav>
   </div><!-- .site-info -->
 </footer><!-- #colophon -->
 
