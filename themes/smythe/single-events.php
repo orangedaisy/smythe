3782a4f379686a9d053aad8541a58c4f4f87e43e
Robert Showalter <robert@orangedaisy.co>
2015-10-04 09:25:11 -0400

    Add 'events' post type and template

---
+++ themes/smythe/single-events.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying all single events posts.
+ *
+ * @package smythe
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+      <article class="o-paper c-telegram">
+
+        <h6 class="c-telegram__header">event telegram</h6>
+
+        <div class="u-row">
+
+          <div class="u-one-third">
+            <img src="<?php the_field('image'); ?>" alt="" class="c-telegram__pic">
+          </div>
+
+          <div class="u-two-thrids">
+            <h2 class="c-telegram__title">
+              <?php echo get_the_title(); ?>
+            </h2>
+            <p class="c-telegram__date"><?php the_field('event_date'); ?></p>
+            <p class="c-telegram__venue">
+              <a href="<?php the_field('venue_website'); ?>" class="c-link">
+                <?php the_field('venue_name'); ?>
+              </a>
+            </p>
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </article>
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
